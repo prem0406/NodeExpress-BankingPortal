75c3df9bbc14bbf73f946b4ec009709bae138730
prem0406 <premk191@gmail.com>
2020-08-06 10:43:15 +0530

    module complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', ejs);
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
