62b8508d67177ee906a5f9e94c41f55874ee45b8
prem0406 <premk191@gmail.com>
2020-08-06 11:24:37 +0530

    module complete

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const accountData = fs.readFileSync('src/json/account.json', {encoding: 'UTF8'});
+const accountData = fs.readFileSync('src/json/accounts.json', {encoding: 'UTF8'});
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync('src/json/user.json', {encoding: 'UTF8'});
+const userData = fs.readFileSync('src/json/users.json', {encoding: 'UTF8'});
 const users = JSON.parse(userData);
 
 app.get('/', (req, res)=> {
